75e55e575666c82b922ed3181212a7ae89a35466
jlord <jess.lord@gmail.com>
2024-10-06 17:03:01 +0100

    Refactored in Sam's way
    Co-authored-by: Sam Cutler <sam.cutler@theguardian.com>

--- index.js
+++ index.js
@@ -2,14 +2,60 @@ const cheerio = require('cheerio')
 const fs = require('fs')
 const path = require('path')
 
+// Missing Family members died between two census years
 const years = ["1900", "1901", "1902", "1903", "1904", "1905", "1906", "1907", "1908", "1909", "1910"]
+
 // Title IDs for local papers
-const titleID = madisonDanielsville
 const madisonDanielsville = "sn89053317" // Danielsville Monitor
 const banksCounty = "sn88054073" // Banks County Journal
 
-const outputDir = path.join(__dirname, titleID)
+// The title ID of the publication we're currently interested in
+const titleID = madisonDanielsville
 
+const outputDir = path.join(__dirname, "output", titleID)
+
+async function getIssueDatesForYear (year) {
+  // Find all of the available issues, as links, within a calendar year
+  const url = `https://gahistoricnewspapers.galileo.usg.edu/lccn/${titleID}/issues/${year}/`
+  const response = await fetch(url)
+  const htmlText = await response.text()
+  const htmlDom = cheerio.load(htmlText)
+  const anchors = htmlDom("a").toArray()
+
+  return anchors
+    .map((e) => e.attribs.href)
+    .filter((h) => h.startsWith(`/issues/${titleID}`))
+    // sn88054073 is the ID for this specific paper (maybe) Banks Count Journal (Homer, Ga)
+    // Example: https://gahistoricnewspapers.galileo.usg.edu/lccn/sn88054073/1900-01-04/ed-1/
+    // isolate just the full issue date: 1900-01-04
+    .map((f) => f.substring(19, 29))
+}
+
+async function getTextPageURLFromIssueDate (issueDate) {
+  const url = `https://gahistoricnewspapers.galileo.usg.edu/issues/${titleID}/${issueDate}/`
+
+  const response = await fetch(url)
+  const htmlText = await response.text()
+  const htmlDom = cheerio.load(htmlText)
+
+  const anchors = htmlDom("a").toArray()
+
+  const regex = new RegExp(`/lccn/${titleID}/${issueDate}/ed-\\d/seq-`)
+
+  return anchors
+    .map((e) => e.attribs.href)
+    .filter((h) => regex.test(h))
+    .map((f) => `https://gahistoricnewspapers.galileo.usg.edu${f}ocr.txt`)
+}
+
+async function saveTextToFile (textURL) {
+  const response = await fetch(textURL)
+  const text = await response.text()
+
+  const filename = textURL.substring(61).replaceAll("/", "_")
+
+  fs.writeFileSync(`${outputDir}/${filename}`, text)
+}
 
 // LET'S GO!!! LOOP THROUGH THE YEARS AND SO FORTH
 async function main() {
@@ -32,42 +78,6 @@ async function main() {
 
 main()
 
-async function getIssueDatesForYear (year) {
-  // Find all of the available issues, as links, within a calendar year
-  const url = `https://gahistoricnewspapers.galileo.usg.edu/lccn/${titleID}/issues/${year}/`
-  const response = await fetch(url)
-  const htmlText = await response.text()
-  const htmlDom = cheerio.load(htmlText)
-  const anchors = htmlDom("a").toArray()
-
-  return anchors
-    .map((e) => e.attribs.href)
-    .filter((h) => h.startsWith(`/issues/${titleID}`))
-    // sn88054073 is the ID for this specific paper (maybe) Banks Count Journal (Homer, Ga)
-    // Example: https://gahistoricnewspapers.galileo.usg.edu/lccn/sn88054073/1900-01-04/ed-1/
-    // isolate just the full issue date: 1900-01-04
-    .map((f) => f.substring(19, 29))
-}
-
-async function getTextPageURLFromIssueDate (issueDate) {
-  const url = `https://gahistoricnewspapers.galileo.usg.edu/issues/${titleID}/${issueDate}/`
-  const response = await fetch(url)
-  const htmlText = await response.text()
-  const htmlDom = cheerio.load(htmlText)
-  const anchors = htmlDom("a").toArray()
-  const regex = new RegExp(`/lccn/${titleID}/${issueDate}/ed-\\d/seq-`)
-  return anchors
-    .map((e) => e.attribs.href)
-    .filter((h) => regex.test(h))
-    .map((f) => `https://gahistoricnewspapers.galileo.usg.edu${f}ocr.txt`)
-}
-
-async function saveTextToFile (textURL) {
-  const response = await fetch(textURL)
-  const text = await response.text()
-  const filename = textURL.substring(61).replaceAll("/", "_")
-  fs.writeFileSync(`${outputDir}/${filename}`, text)
-}
 
 
 // Year Page https://gahistoricnewspapers.galileo.usg.edu/lccn/sn88054073/issues/1900/
